6cafdee354c6ef687176a9bbcb86ba11386cea26
Axel-N'CHO <naxelamour09+github@gmail.com>
2024-11-12 18:06:40 +0100

    Part 3 - Phonebook until 3.14 included - removed hardcoded phonebook

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -23,28 +23,28 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
-const phonebookEntries = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// const phonebookEntries = [
+//     {
+//         "id": "1",
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": "2",
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": "3",
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": "4",
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 // const generateId = () => {
 //     const minCeiled = Math.ceil(5);
